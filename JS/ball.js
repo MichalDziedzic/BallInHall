cf89712713e71aa8cdb1bea5c201debdaad75e14
MichalDziedzic <michaldziedzic8@gmail.com>
2020-03-06 19:15:00 +0100

    generate holes on board

--- JS/ball.js
+++ JS/ball.js
@@ -3,15 +3,18 @@ class Ball {
        // this.ball=document.querySelector('.ball'),
         this.svgBallBoard=document.querySelector('#BallBoard'),
         this.playGround= document.querySelector('.playGround'),
+        this.svgBall=document.querySelector('#svgBallelem'),
         this.health=3,
         this.finishBallGame=finishGame;
         this.posX=window.innerWidth/2;
         this.posY=window.innerHeight/2;
         this.circle={};
+        this.tabHall=[{},{}];
         this.radius=10;
-        this.amount=0;
+        this.amount=1;
        // console.log('test4');
        this.startBallProperties();
+       this.generateHoleOnBoard();
         this.moveEvent = window.addEventListener('deviceorientation', (e) =>
         {
           this.BallSpeed(e.gamma, e.beta)
@@ -27,10 +30,12 @@ class Ball {
 
     startBallProperties(){
 
-       // console.log('elo');
+   
         const svgBallElem=document.createElementNS('http://www.w3.org/2000/svg','circle');
+
+       
         this.circle.el = svgBallElem.cloneNode(false);
-       // console.log(this.circle);
+        
     
         this.circle.el.setAttribute('cx',this.posX);
         this.circle.el.setAttribute('cy',this.posY);
@@ -39,8 +44,39 @@ class Ball {
         this.svgBallBoard.appendChild(this.circle.el);
         // console.log(this.svgBallBoard); 
 
-}
-  
+    }
+    
+    generateHoleOnBoard()
+    {
+
+        const rangeH=window.innerWidth;
+        const rangeW=window.innerWidth;
+
+        const svghall=document.createElementNS('http://www.w3.org/2000/svg','circle');
+
+         for(let i=0;i<this.tabHall.length;i++)
+        {
+            this.tabHall[i].el=svghall.cloneNode(false);
+            
+           let randPosX=Math.floor(Math.random()*(rangeW-30)+30);
+           let randPosY=Math.floor(Math.random()*(rangeH-30)+30);
+           this.tabHall[i].posX=randPosX;
+           this.tabHall[i].posY=randPosY;
+
+           
+            this.tabHall[i].el.setAttribute('cx',randPosX);
+            this.tabHall[i].el.setAttribute('cy',randPosY);
+            this.tabHall[i].el.setAttribute('r', this.radius+5);
+            this.tabHall[i].el.setAttribute('id',`svgHall${i}`);
+           // this.svgBall.after(this.tabHall[i].el);
+           this.svgBallBoard.appendChild(this.tabHall[i].el);
+           console.log(this.tabHall[i].posX);
+
+        
+         }    
+        
+        
+    }
     
   
     moveBall(){
@@ -65,7 +101,20 @@ class Ball {
         //     this.stopBallGame();
         // }
      
+        for(let i=0;i<this.tabHall.length;i++)
+        {
+            //this.posX-5<=this.tabHall[i]randPosX && this.posX+5>this.tabHall[i] || this.posX===this.tabHall[i]
+            //this.x>=25 && this.x<=53
+            if(Math.floor(this.posX)>=(this.tabHall[i].randPosX) && Math.floor(this.posX)<=(this.tabHall[i].randPosX)+5)
+            {
+                console.log('work!');
+            }
+       
+         
+           console.log(this.tabHall[i].posX);
+
         
+         }    
         
 
 
